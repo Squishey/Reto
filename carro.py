b439018675c99e2b78ce6a2702b9f3701eecfcf2
gonzagoc <a01736865@tec.mx>
2026-03-08 20:18:24 -0600

    coche

--- carro.py
+++ carro.py
@@ -1,74 +1,26 @@
 import numpy as np
-import pygame
-from config import AZUL, ACELERACION, FRENADO, DISTANCIA_COLISION, DISTANCIA_SEMAFORO, ANCHO_PANTALLA
+
+A = np.array([
+[1,2,-3,4,5],
+[-2,-5,8,-8,-9],
+[1,2,-2,7,9],
+[1,1,0,6,12],
+[2,4,-6,8,11]
+])
+
+A_inv = np.linalg.inv(A)
+print(A_inv)
 
 
-class Carro:
-    def __init__(self, x, y, velocidad_inicial, velocidad_maxima, color=AZUL, ancho=40, alto=20):
-        self.posicion = np.array([x, y], dtype=float)
-        self.velocidad = velocidad_inicial
-        self.velocidad_maxima = velocidad_maxima
-        self.color = color
-        self.ancho = ancho
-        self.alto = alto
-
-    # Logica movimiento
-    def _debe_frenar(self, semaforo, otros_carros):
-        dist_sem = self._distancia(self.posicion, semaforo.posicion)
-        cerca = dist_sem < DISTANCIA_SEMAFORO
-        antes = self.posicion[0] < semaforo.posicion[0]
-
-        if semaforo.estado == "rojo" and cerca and antes:
-            return True
-
-        for otro in otros_carros:
-            if otro is self:
-                continue
-            if otro.posicion[0] > self.posicion[0]:
-                if otro.posicion[0] - self.posicion[0] < DISTANCIA_COLISION:
-                    return True
-
-        return False
-
-    def actualizar(self, semaforo, otros_carros):
-        if self._debe_frenar(semaforo, otros_carros):
-            self.velocidad -= FRENADO
-            if self.velocidad < 0:
-                self.velocidad = 0
-        else:
-            self.velocidad += ACELERACION
-            if self.velocidad > self.velocidad_maxima:
-                self.velocidad = self.velocidad_maxima
-
-        self.posicion[0] += self.velocidad
-
-        # Reinicio al salir de pantalla
-        if self.posicion[0] > ANCHO_PANTALLA + 40:
-            self.posicion[0] = -40
-
-# ------------------------------------------------------------!
-    # Dibujar con matriz de rotación (etapa 2)
-# -----------------------------------------------------------!
-    def dibujar(self, pantalla, angulo=0):
-        vertices = np.array([
-            [-self.ancho / 2, -self.alto / 2],
-            [ self.ancho / 2, -self.alto / 2],
-            [ self.ancho / 2,  self.alto / 2],
-            [-self.ancho / 2,  self.alto / 2]
-        ])
-
-        rad = np.radians(angulo)
-        R = np.array([
-            [np.cos(rad), -np.sin(rad)],
-            [np.sin(rad),  np.cos(rad)]
-        ])
-
-        rotado     = vertices @ R.T
-        trasladado = rotado + self.posicion
-
-        pygame.draw.polygon(pantalla, self.color, trasladado)
-
-
-    def _distancia(self, p1, p2):
-        d = p1 - p2
-        return np.sqrt(d[0] ** 2 + d[1] ** 2)
+M = np.array([
+[68,116,-123,381,559],
+[98,161,-172,521,812],
+[34,21,25,208,410],
+[79,131,-157,375,587],
+[42,47,-32,214,382],
+[80,140,-176,384,559],
+[18,7,30,144,242],
+[23,32,-26,129,208]
+])
+resultado= M@A_inv
+print(resultado)
